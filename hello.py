3b5c8ecb4e37a627d07edfe56bd849eb4f1d9342
imguagua <skyfan1981@gmail.com>
2016-02-26 13:10:16 +0800

    modify hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.script import Manager
 
 app = Flask(__name__)
@@ -12,8 +12,10 @@ def index():
 def user(username):
     return "<p> hell,%s </p>" %username
 
+@app.route("/html/<username>")
+def html(username):
+    return render_template('index.html', name=username)
+
 
 if __name__ == "__main__":
     manager.run()
-
-    
