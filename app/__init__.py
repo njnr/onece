df3fb2983500f197eeeedf086b45d2efa21b60f4
imguagua <skyfan1981@gmail.com>
2016-03-17 15:54:53 +0800

    login

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ db = SQLAlchemy()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
-login.login_view = 'auth.login'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validate_on_submit import Required, Length, Email
+from wtforms.validators import Required, Length, Email
 
 class LoginForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64),
