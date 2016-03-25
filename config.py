f65fe4a6f19d2b52e69f2c8cc554b511c7945ae7
imguagua <skyfan1981@gmail.com>
2016-03-25 11:11:49 +0800

    update config

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'skyfan1981@gmail.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'singleMood'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <skyfan1981@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
