33213de84739babe918b101637c1d83af3f45669
imguagua <skyfan1981@gmail.com>
2016-03-25 11:05:15 +0800

    update manage.py add loaddb

--- manage.py
+++ manage.py
@@ -22,6 +22,10 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
-
+@manager.command
+def loaddb():
+    db.drop_all()
+    db.create_all()
+    
 if __name__ == '__main__':
     manager.run()
