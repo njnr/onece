81725bb6c45b0cb446a86c62cfbf83303db722ec
imguagua <skyfan1981@gmail.com>
2016-03-28 11:52:45 +0800

    add change_email reset_password

--- app/email.py
+++ app/email.py
@@ -11,9 +11,6 @@ def send_async_email(app, msg):
 
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
-
-    print app.config['MAIL_PASSWORD'] + '------++++++++++++'
-    print app.config['MAIL_USERNAME'] + '------++++++++++++'
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
