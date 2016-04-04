961757ead5a303c5a4d1f8adda668a3fe705551a
imguagua <skyfan1981@gmail.com>
2016-04-04 21:21:05 +0800

    add user edit

--- app/main/views.py
+++ app/main/views.py
@@ -1,39 +1,23 @@
 # coding: utf-8
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, redirect, url_for, abort, flash
 from .. import db
-from ..models import User, Permission
-from ..email import send_email
+from ..models import User, Role 
 from . import main
-from .forms import NameForm
-from ..decorators import admin_required, permission_required
-from flask.ext.login import login_required
+from .forms import EditProfileForm, EditProfileAdminForm
+from ..decorators import admin_required
+from flask.ext.login import login_required, current_user
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            """
-            if current_app.config['FLASKY_ADMIN']:
-               send_email(current_app.config['FLASK_ADMIN'], 'New User',
-                           'mail/new_user', user=user) """
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False))
+    return render_template('index.html')
 
+    
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user = user)
+
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
@@ -49,24 +33,30 @@ def edit_profile():
     form.name.data = current_user.name
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
 
-
-
-
-@main.route('/read')
-def read():
-    return "这是我女儿11天的时候拍的照片，小脚丫超级可爱呀！"
-
-
-
-@main.route('/admin')
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
 @login_required
 @admin_required
-def for_admins_only():
-    return "for administrators!"
-
-@main.route('/moderator')
-@login_required
-@permission_required(Permission.MODERATE_COMMENTS)
-def for_moderators_only():
-    return "for comment moderators!"
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
